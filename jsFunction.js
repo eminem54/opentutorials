d97de881de9e7feb1a7a988eae6b6256efb34167
eminem54 <eminem54@naver.com>
2018-08-27 22:46:23 +0900

    asd

---
+++ jsFunction.js
@@ -0,0 +1,39 @@
+function nightDayHandler(self){
+    var target = document.querySelector('body');
+    if(self.value === 'night'){
+    Body.setBackGroundColor('black');
+    Body.setColor('white');
+    self.value='day';
+    Links.setColor('powderblue');
+  } else {
+    Body.setBackGroundColor('white');
+    Body.setColor('black');
+    self.value = 'night';
+    Links.setColor('blue');
+    
+  }
+}
+
+var Links = {
+    setColor:function(color){
+    // var alist = document.querySelectorAll('a');
+    // var i = 0;
+    // while(i<alist.length){
+    //   alist[i].style.color = color;
+    //   i= i+1;
+    // }
+    $('a').css('color', color);
+  }
+}
+
+var Body = {
+  setColor:function (color){
+    //document.querySelector('body').style.color = color;
+    $('body').css('color',color);
+  },
+  setBackGroundColor:function (color){
+    //document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor',color);
+  }
+}
+
